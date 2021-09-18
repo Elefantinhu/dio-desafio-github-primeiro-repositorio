0b810d20425fc03ee7a36130b17e2daa7ce901fb
elefantinhu <rafaeloliveiracanal@gmail.com>
2021-09-18 14:24:59 -0300

    AulaSobreIDE

---
+++ Primeira Semana/Dia 2/entendendo-sobre-ide/src/PrimeiroContatoIDE.java
@@ -0,0 +1,21 @@
+import TesteDePacotes.Gato;
+
+public class PrimeiroContatoIDE {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Livros Livros = new Livros();
+		
+		/*int a = 2;
+		int b = 5;
+		System.out.println("Hello World! " + (a+b));*/
+	}
+
+}
+
+
+class Livros {
+	 private String nome;
+	 private String npag;
+}
